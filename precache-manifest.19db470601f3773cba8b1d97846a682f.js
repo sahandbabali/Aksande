82893fcff1d6b32f2e3e93de7c2ac42bcfc24c06
Sahand Babali <sahand.babali@gmail.com>
2019-11-08 21:17:12 +0330

    Updates

---
+++ precache-manifest.19db470601f3773cba8b1d97846a682f.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16a057c355b810cb87f504b0fa953954",
+    "url": "/aksande/index.html"
+  },
+  {
+    "revision": "f56d70d9e90b16400833",
+    "url": "/aksande/static/css/main.34f9ee70.chunk.css"
+  },
+  {
+    "revision": "5fd94a22945353688410",
+    "url": "/aksande/static/js/2.7557ee2d.chunk.js"
+  },
+  {
+    "revision": "f56d70d9e90b16400833",
+    "url": "/aksande/static/js/main.8d0d2961.chunk.js"
+  },
+  {
+    "revision": "accd98179613207a2422",
+    "url": "/aksande/static/js/runtime-main.6231d7a7.js"
+  },
+  {
+    "revision": "e2742fc9dcaf7bcd612176e3ff0928a0",
+    "url": "/aksande/static/media/5cb4839d5f1b6d3fbadece7c.e2742fc9.png"
+  },
+  {
+    "revision": "0b5055ac357359f8c23320ea3dc0f78b",
+    "url": "/aksande/static/media/IRANSansWeb.0b5055ac.woff2"
+  },
+  {
+    "revision": "66b5cb30e2efe84dab915617acd39041",
+    "url": "/aksande/static/media/IRANSansWeb.66b5cb30.woff"
+  },
+  {
+    "revision": "c26a9eb202b3ef6455281f0335cfffe9",
+    "url": "/aksande/static/media/IRANSansWeb.c26a9eb2.ttf"
+  },
+  {
+    "revision": "d3931868176480f01fd4524cbd48b667",
+    "url": "/aksande/static/media/IRANSansWeb.d3931868.eot"
+  },
+  {
+    "revision": "1becfaba43eef43af02099d64f0590a6",
+    "url": "/aksande/static/media/IRANSansWeb_Black.1becfaba.ttf"
+  },
+  {
+    "revision": "c0f5dbbde9104d1dc813b68f50457e2c",
+    "url": "/aksande/static/media/IRANSansWeb_Black.c0f5dbbd.eot"
+  },
+  {
+    "revision": "dfb26a2f06ef268a82147b6ea001bcf9",
+    "url": "/aksande/static/media/IRANSansWeb_Black.dfb26a2f.woff"
+  },
+  {
+    "revision": "f477dd9634c960c5ce215aa435e32b4c",
+    "url": "/aksande/static/media/IRANSansWeb_Black.f477dd96.woff2"
+  },
+  {
+    "revision": "43a0ecf3c7f2af819b192d1284f95ed9",
+    "url": "/aksande/static/media/IRANSansWeb_Bold.43a0ecf3.woff2"
+  },
+  {
+    "revision": "e4ed975d7eb3ca8da589f27e55ef7f20",
+    "url": "/aksande/static/media/IRANSansWeb_Bold.e4ed975d.woff"
+  },
+  {
+    "revision": "ebec177a9eccde9d5a4fb510ff1d1d8e",
+    "url": "/aksande/static/media/IRANSansWeb_Bold.ebec177a.ttf"
+  },
+  {
+    "revision": "f2cb5d5906f5abc3c9f59e561b007ded",
+    "url": "/aksande/static/media/IRANSansWeb_Bold.f2cb5d59.eot"
+  },
+  {
+    "revision": "28bc7e37dc1bba427c24941b19a11e4a",
+    "url": "/aksande/static/media/IRANSansWeb_Light.28bc7e37.eot"
+  },
+  {
+    "revision": "63f8b4b6f55a927231311a9e83cb4fda",
+    "url": "/aksande/static/media/IRANSansWeb_Light.63f8b4b6.woff"
+  },
+  {
+    "revision": "e8ff3f7167961a35e98db2fd15bb37ca",
+    "url": "/aksande/static/media/IRANSansWeb_Light.e8ff3f71.ttf"
+  },
+  {
+    "revision": "f7e4be98d20eb763b867143da5207b90",
+    "url": "/aksande/static/media/IRANSansWeb_Light.f7e4be98.woff2"
+  },
+  {
+    "revision": "0c40b1a0ee0e7fedeb8a4ac6c58e270b",
+    "url": "/aksande/static/media/IRANSansWeb_Medium.0c40b1a0.woff"
+  },
+  {
+    "revision": "1da39fcf35d32c87ce9b3bdcfd2b90b2",
+    "url": "/aksande/static/media/IRANSansWeb_Medium.1da39fcf.ttf"
+  },
+  {
+    "revision": "3d468556450b88fba2ef7ac695a166d3",
+    "url": "/aksande/static/media/IRANSansWeb_Medium.3d468556.eot"
+  },
+  {
+    "revision": "9c66b762719d40d1f18e678a1405459a",
+    "url": "/aksande/static/media/IRANSansWeb_Medium.9c66b762.woff2"
+  },
+  {
+    "revision": "068169d2ab5f8a5dd1beae8b0149217d",
+    "url": "/aksande/static/media/IRANSansWeb_UltraLight.068169d2.woff2"
+  },
+  {
+    "revision": "3a70f4b4dcc84bcb84c3749ec235338d",
+    "url": "/aksande/static/media/IRANSansWeb_UltraLight.3a70f4b4.eot"
+  },
+  {
+    "revision": "51b9c1ad55d006c89ec0639de7892278",
+    "url": "/aksande/static/media/IRANSansWeb_UltraLight.51b9c1ad.ttf"
+  },
+  {
+    "revision": "bb7ec39fffa2e3ce96a5f52edd9e13c4",
+    "url": "/aksande/static/media/IRANSansWeb_UltraLight.bb7ec39f.woff"
+  },
+  {
+    "revision": "9c0832c50d938c8c17170d902b945a03",
+    "url": "/aksande/static/media/Pixabay_new_logo.svg.9c0832c5.png"
+  },
+  {
+    "revision": "c9014a627ad09f23ef8bf17a9ce2058e",
+    "url": "/aksande/static/media/pexels-logo-7e4af4630e66b6b786567041874586aeb1b5217589035c70a0def15aacd0f11a.c9014a62.png"
+  }
+]);
